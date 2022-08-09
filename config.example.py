56bbcf0259d8717b244000b03f00e1509b5aa131
Emoji <55009343+Emojigit@users.noreply.github.com>
2022-08-09 12:27:24 +0800

    rewrite in telethon

---
+++ config.example.py
@@ -0,0 +1,11 @@
+import logging
+
+# Get the following from my.telegram.org
+api_id = 1234567
+api_hash = 'jrshbdughrseuhncudmcgusegryu'
+
+# Get the folloing from @botfather
+bot_token = '9275476:hnseuvghehrcgu'
+
+logging_level = logging.INFO
+logging_format = "%(asctime)s %(levelname)s[%(name)s]: %(message)s"
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from telebot import *
-from random import randint
-bot = TeleBot("在这填上令牌", parse_mode=None)
-@bot.message_handler(commands=['tossCoin'])
-def send_coin(message):
-    if randint(0,1) == 0:
-        bot.reply_to(message,"硬币是反面喵！")
-    else:
-        bot.reply_to(message,"硬币是正面喵！")
-bot.infinity_polling()
+from telethon import TelegramClient, functions, types, events, utils
+from telethon.errors import *
+import logging, config, random
+
+logging.basicConfig(level=config.logging_level,format=config.logging_format)
+log = logging.getLogger(__name__)
+
+bot = TelegramClient('coinBot', config.api_id, config.api_hash).start(bot_token=config.bot_token)
+
+choices = ["硬币是反面喵！","硬币是正面喵！"]
+
+@bot.on(events.NewMessage(pattern='/tosscoin'))
+async def checkpriv(event):
+    await event.respond(random.choice(choices))
+    log.info("User {} executed /tosscoin".format(event.sender.id))
+    raise events.StopPropagation
+
+log.info("Finished. Starting bot...")
+bot.run_until_disconnected()
